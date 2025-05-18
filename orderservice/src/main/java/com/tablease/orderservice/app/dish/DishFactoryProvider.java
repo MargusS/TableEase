2df21c4489e0e254d3d709b9985ba86b2acd9167
marguss <agusnicolas21@gmail.com>
2025-05-18 21:27:37 +0100

    ➕Add Dish Factory

---
+++ orderservice/src/main/java/com/tablease/orderservice/app/dish/DishFactoryProvider.java
@@ -0,0 +1,24 @@
+package com.tablease.orderservice.app.dish;
+
+import com.tablease.orderservice.domain.dish.factory.DishFactory;
+import com.tablease.orderservice.domain.dish.DishType;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class DishFactoryProvider {
+    private final Map<String, DishFactory> factories;
+    public DishFactoryProvider(List<DishFactory> factoryList) {
+        this.factories = factoryList.stream()
+                .collect(Collectors.toMap(DishFactory::getSupportedFactory, Function.identity()));
+    }
+
+    public DishFactory getFactory(DishType type) {
+        String key = type.getName().toUpperCase();
+        DishFactory factory = factories.get(key);
+
+        return factory;
+    }
+}
---
+++ orderservice/src/main/java/com/tablease/orderservice/domain/dish/factory/BeverageDishFactory.java
@@ -0,0 +1,22 @@
+package com.tablease.orderservice.domain.dish.factory;
+
+import com.tablease.orderservice.domain.dish.Dish;
+import com.tablease.orderservice.domain.dish.DishType;
+import com.tablease.orderservice.domain.dish.valueobjects.Allergen;
+import com.tablease.orderservice.domain.dish.valueobjects.DishId;
+import com.tablease.orderservice.domain.dish.valueobjects.Price;
+
+import java.time.Instant;
+import java.util.Set;
+
+public class BeverageDishFactory implements DishFactory {
+    @Override
+    public Dish create(DishId id, String name, String description, Set<Allergen> allergens, boolean isActive, Price price, Price cost, DishType type, Instant createdAt, String thumbnailUrl) {
+        return new Dish(id, name,description,allergens, isActive, price, cost, type, createdAt, thumbnailUrl);
+    }
+
+    @Override
+    public String getSupportedFactory(){
+        return "BEVERAGE";
+    }
+}
