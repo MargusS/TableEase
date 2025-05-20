07e789ee3f67f1e373cbd99c642e5b5b7a8bec2b
marguss <agusnicolas21@gmail.com>
2025-05-20 21:11:53 +0100

    ➕ Dish Repo

--- orderservice/src/main/java/com/tablease/orderservice/domain/dish/repository/DishRepository.java
+++ orderservice/src/main/java/com/tablease/orderservice/domain/dish/repository/DishRepository.java
@@ -1,11 +1,19 @@
 package com.tablease.orderservice.domain.dish.repository;
 
 import com.tablease.orderservice.domain.dish.Dish;
-import com.tablease.orderservice.domain.dish.valueobjects.DishId;
 
 import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
 
 public interface DishRepository {
     Dish save(Dish dish);
-    Optional<Dish> findById(DishId id);
+
+    Optional<Dish> findById(UUID dishId);
+
+    Set<Dish> findAll();
+
+    Dish update(Dish dish);
+
+    void delete(Dish dish);
 }
