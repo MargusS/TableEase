51d5fe41c9dd01d18eed16f182e474e119fa2a28
marguss <agusnicolas21@gmail.com>
2025-05-24 10:29:33 +0100

    ➕ Dish DB entity and repo

---
+++ orderservice/src/main/java/com/tablease/orderservice/infra/db/jpa/entities/dish/DishEntity.java
@@ -0,0 +1,41 @@
+package com.tablease.orderservice.infra.db.jpa.entities.dish;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "dishes")
+public class DishEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, columnDefinition = "uuid")
+    private UUID uuid;
+
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private BigDecimal cost;
+    private boolean isActive;
+    private String thumbnail;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "dish_type_id")
+    private DishTypeEntity dishType;
+
+    @ManyToMany
+    @JoinTable(
+            name = "dish_allergens",
+            joinColumns = @JoinColumn(name = "dish_id"),
+            inverseJoinColumns = @JoinColumn(name = "allergen_id")
+    )
+    private List<AllergenEntity> allergens;
+
+    private Instant createdAt;
+    private Instant updatedAt;
+}
