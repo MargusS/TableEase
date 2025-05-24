6a3ac4bb5d739b794d484790e6ce2b80d605881e
marguss <agusnicolas21@gmail.com>
2025-05-24 10:27:51 +0100

    ➕ Add DishBoundariesImpl with create

---
+++ orderservice/src/main/java/com/tablease/orderservice/app/dish/usecases/DishBoundariesImpl.java
@@ -0,0 +1,63 @@
+package com.tablease.orderservice.app.dish.usecases;
+
+import com.tablease.orderservice.app.dish.dto.DishRequest;
+import com.tablease.orderservice.app.dish.dto.DishResponse;
+import com.tablease.orderservice.app.dish.presenter.DishPresenter;
+import com.tablease.orderservice.domain.dish.Allergen;
+import com.tablease.orderservice.domain.dish.Dish;
+import com.tablease.orderservice.domain.dish.DishType;
+import com.tablease.orderservice.domain.dish.factory.DishFactory;
+import com.tablease.orderservice.domain.dish.factory.DishFactoryProvider;
+import com.tablease.orderservice.domain.dish.repository.AllergenRepository;
+import com.tablease.orderservice.domain.dish.repository.DishRepository;
+import com.tablease.orderservice.domain.dish.repository.DishTypeRepository;
+import com.tablease.orderservice.domain.dish.valueobjects.Price;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class DishBoundariesImpl implements DishBoundaries {
+
+    private final DishFactoryProvider dishFactoryProvider;
+    private final DishPresenter dishPresenter;
+    private final DishRepository dishRepository;
+    private final AllergenRepository allergenRepository;
+    private final DishTypeRepository dishTypeRepository;
+
+    public DishBoundariesImpl(DishFactoryProvider dishFactoryProvider, DishPresenter dishPresenter, DishRepository dishRepository, AllergenRepository allergenRepository, DishTypeRepository dishTypeRepository) {
+        this.dishFactoryProvider = dishFactoryProvider;
+        this.dishPresenter = dishPresenter;
+        this.dishRepository = dishRepository;
+        this.allergenRepository = allergenRepository;
+        this.dishTypeRepository = dishTypeRepository;
+    }
+
+    @Override
+    public DishResponse createDish(DishRequest request) {
+
+        DishType dishType = dishTypeRepository.findById(request.dishTypeId()).orElse(null);
+        if (dishType == null) {
+            return dishPresenter.error("Invalid dish type");
+        }
+
+        Set<Allergen> allergens = allergenRepository.findAllByUUID(request.allergenIds());
+
+        DishFactory factory = dishFactoryProvider.getFactory(dishType);
+        Dish dish = factory.create(request.name(),
+                request.description(),
+                allergens,
+                request.isActive(),
+                new Price(request.price()),
+                new Price(request.cost()),
+                dishType,
+                request.thumbnailUrl());
+
+        Dish savedDish = dishRepository.save(dish);
+
+        if (savedDish == null) {
+            return dishPresenter.error("Failed to create dish");
+        }
+        return dishPresenter.success(savedDish);
+    }
+}
