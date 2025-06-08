d25aad6515dc313fa84e1db55d5672e86f2732a3
marguss <agusnicolas21@gmail.com>
2025-06-08 18:10:15 +0100

    🗑️ Delete DishId

--- orderservice/src/main/java/com/tablease/orderservice/domain/dish/Dish.java
+++ orderservice/src/main/java/com/tablease/orderservice/domain/dish/Dish.java
@@ -1,38 +1,26 @@
 package com.tablease.orderservice.domain.dish;
 
-import com.tablease.orderservice.domain.dish.valueobjects.DishId;
 import com.tablease.orderservice.domain.dish.valueobjects.Price;
-import lombok.Getter;
 
 import java.time.Instant;
 import java.util.List;
+import java.util.UUID;
 
-@Getter
-public class Dish {
-    private final DishId id;
-    private final Instant createdAt;
-    private Instant updatedAt;
-    private final String name;
-    private final String description;
-    private final List<Allergen> allergens;
-    private final boolean isActive;
-    private final Price price;
-    private final Price cost;
-    private final String thumbnailUrl;
-    private final DishType type;
-
-    public Dish(DishId id, String name, String description, List<Allergen> allergens, boolean isActive, Price price, Price cost, DishType type, String thumbnailUrl) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.allergens = allergens;
-        this.isActive = isActive;
-        this.price = price;
-        this.cost = cost;
-        this.type = type;
-        this.createdAt = Instant.now();
-        this.thumbnailUrl = thumbnailUrl;
-        this.updatedAt = this.createdAt;
+public record Dish(
+        UUID uuid,
+        Instant createdAt,
+        Instant updatedAt,
+        String name,
+        String description,
+        List<Allergen> allergens,
+        boolean isActive,
+        Price price,
+        Price cost,
+        String thumbnailUrl,
+        DishType type
+) {
+    public Dish(UUID uuid, String name, String description, List<Allergen> allergens,
+                boolean isActive, Price price, Price cost, DishType type, String thumbnailUrl) {
+        this(uuid, Instant.now(), Instant.now(), name, description, allergens, isActive, price, cost, thumbnailUrl, type);
     }
-
 }
