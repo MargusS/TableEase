0a4a2ffdc9868627dfac354c62976bfe99901229
marguss <agusnicolas21@gmail.com>
2025-06-08 18:07:36 +0100

    🗑️ Delete AllergenId

--- orderservice/src/main/java/com/tablease/orderservice/domain/dish/Allergen.java
+++ orderservice/src/main/java/com/tablease/orderservice/domain/dish/Allergen.java
@@ -1,24 +1,9 @@
 package com.tablease.orderservice.domain.dish;
 
-import com.tablease.orderservice.domain.dish.valueobjects.AllergenId;
-import lombok.Getter;
+import java.util.UUID;
 
-@Getter
-public class Allergen {
-    private final AllergenId id;
-    private final String name;
-    private final String symbolUrl; // path or full URL to image/icon
-    private final String description;
-
-    public Allergen(AllergenId id, String name, String symbolUrl, String description) {
+public record Allergen(UUID uuid, String name, String symbolUrl, String description) {
+    public Allergen {
         if (name == null || name.isBlank()) throw new IllegalArgumentException("Allergen name required");
-        if (symbolUrl == null || symbolUrl.isBlank()) throw new IllegalArgumentException("Allergen symbol required");
-        if (description == null || description.isBlank())
-            throw new IllegalArgumentException("Allergen description required");
-
-        this.id = id;
-        this.name = name;
-        this.symbolUrl = symbolUrl;
-        this.description = description;
     }
 }
