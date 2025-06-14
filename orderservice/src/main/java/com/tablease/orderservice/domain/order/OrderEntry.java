371748d4d28a4373dc0a51da8bc35dc2da9a5cf2
marguss <agusnicolas21@gmail.com>
2025-06-14 20:24:13 +0100

    ♻️ Update dishId for order

--- orderservice/src/main/java/com/tablease/orderservice/domain/order/OrderEntry.java
+++ orderservice/src/main/java/com/tablease/orderservice/domain/order/OrderEntry.java
@@ -1,19 +1,19 @@
 package com.tablease.orderservice.domain.order;
 
 import com.tablease.orderservice.domain.dish.valueobjects.Destination;
-import com.tablease.orderservice.domain.dish.valueobjects.DishId;
 import com.tablease.orderservice.domain.dish.valueobjects.Price;
 import com.tablease.orderservice.domain.order.valueobjects.OrderEntryId;
 import com.tablease.orderservice.domain.order.valueobjects.Quantity;
 import lombok.Getter;
 
 import java.time.Instant;
+import java.util.UUID;
 
 @Getter
 public class OrderEntry {
 
 	private final OrderEntryId id;
-	private final DishId dishId;
+	private final UUID dishId;
 	private final String dishName;
 	private final Price dishPriceAtOrder;
 	private final Quantity quantity;
@@ -21,7 +21,7 @@ public class OrderEntry {
 	private final Instant createdAt;
 	private Instant updatedAt;
 
-	public OrderEntry(OrderEntryId id, DishId dishId, String dishName, Price price, Quantity quantity, Destination dishDestination,Instant createdAt) {
+	public OrderEntry(OrderEntryId id, UUID dishId, String dishName, Price price, Quantity quantity, Destination dishDestination,Instant createdAt) {
 		this.id = id;
 		this.dishId = dishId;
 		this.dishName = dishName;
