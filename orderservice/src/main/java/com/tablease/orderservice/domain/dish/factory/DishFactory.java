2c738077475c8e22adc153de07801c8bc2754575
marguss <agusnicolas21@gmail.com>
2025-06-24 19:21:37 +0100

    ♻️ Update dish factory

--- orderservice/src/main/java/com/tablease/orderservice/domain/dish/factory/DishFactory.java
+++ orderservice/src/main/java/com/tablease/orderservice/domain/dish/factory/DishFactory.java
@@ -7,9 +7,7 @@ import com.tablease.orderservice.domain.dish.valueobjects.CostPrice;
 import com.tablease.orderservice.domain.dish.valueobjects.SellingPrice;
 
 import java.util.List;
-import java.util.UUID;
 
 public interface DishFactory {
-
     Dish create(String name, String description, SellingPrice price, CostPrice cost, DishType dishType, List<Allergen> allergens, String thumbnail);
 }
