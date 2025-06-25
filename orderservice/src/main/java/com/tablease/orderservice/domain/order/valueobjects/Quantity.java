dc071824e1472b70351033dbdb243695479254d2
marguss <agusnicolas21@gmail.com>
2025-06-25 20:46:51 +0100

    🗑️ Delete unused VO

--- orderservice/src/main/java/com/tablease/orderservice/domain/order/valueobjects/Quantity.java
+++
@@ -1,9 +0,0 @@
-package com.tablease.orderservice.domain.order.valueobjects;
-
-public record Quantity(int value) {
-    public Quantity {
-        if (value <= 0)
-            throw new IllegalArgumentException("Quantity must be positive");
-    }
-
-}
