4068cbd6d8a3db791a24ffc82e2980b9971cfd6d
marguss <agusnicolas21@gmail.com>
2025-06-25 20:47:09 +0100

    ➕ Add OrderStatus VO

---
+++ orderservice/src/main/java/com/tablease/orderservice/domain/order/valueobjects/OrderStatus.java
@@ -0,0 +1,10 @@
+package com.tablease.orderservice.domain.order.valueobjects;
+
+public enum OrderStatus {
+    CREATED,
+    RECEIVED,
+    IN_PROGRESS,
+    READY,
+    DELIVERED,
+    CANCELLED
+}
--- orderservice/src/main/java/com/tablease/orderservice/domain/order/valueobjects/Status.java
+++
@@ -1,8 +0,0 @@
-package com.tablease.orderservice.domain.order.valueobjects;
-
-public enum Status {
-	CREATED,
-	IN_PROGRESS,
-	COMPLETED,
-	CANCELLED
-}
